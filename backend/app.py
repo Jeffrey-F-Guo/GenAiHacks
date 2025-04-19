226fb8979c5dd348b66aea3e03eef35163d962af
Jeffrey Guo <jeffreyg@Jeffreys-MacBook-Pro.local>
2025-04-19 09:34:08 -0700

    adding boilerplate flask

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,17 @@
 from flask import Flask
+import requests
 from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return {"message":'Hello, World!'}
 
 if __name__ == '__main__':
     app.run(debug=True)
